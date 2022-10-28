7330e1b222941b124f894e9dcb954748d51affec
Tasfiajahan <tania.develpers@gmail.com>
2022-10-28 12:27:01 +0600

    Thirt commit

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -10,7 +10,7 @@ const HeroSection = () => {
                     <img src={homeimg} className="max-w-sm rounded-lg shadow-2xl" alt=''/>
                     <div>
                         <h1 className="text-5xl font-bold">MD EDU Academy</h1>
-                        <p className="py-6">This websit you can learn about React, Vew, Html, Css, JavaScript, Bootstrap, WordPress and ETC.</p>
+                        <p className="py-6">This website you can learn about React, Vew, Html, Css, JavaScript, Bootstrap, WordPress and ETC.</p>
                         <button className="text-white text-sm px-4 py-2 rounded bg-yellow-700"> <Link to="/course" className='text-black no-underline text-bold'>Course</Link> </button>
                     </div>
                 </div>
