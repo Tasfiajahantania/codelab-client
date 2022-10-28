76cde896f24b28116c6a4225ccc41c43fb2c6fb1
Tasfiajahan <tania.develpers@gmail.com>
2022-10-28 13:19:39 +0600

    my 5th commit

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -9,9 +9,9 @@ const HeroSection = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={homeimg} className="max-w-sm rounded-lg shadow-2xl" alt=''/>
                     <div>
-                        <h1 className="text-5xl font-bold">MD EDU Academy</h1>
+                        <h1 className="text-5xl font-bold">CodeLab</h1>
                         <p className="py-6">This website you can learn about React, Vew, Html, Css, JavaScript, Bootstrap, WordPress and ETC.</p>
-                        <button className="text-white text-sm px-4 py-2 rounded bg-yellow-700"> <Link to="/course" className='text-black no-underline text-bold'>Course</Link> </button>
+                        <button className="text-white text-sm px-4 py-2 rounded bg-pink-500"> <Link to="/course" className='text-black no-underline text-bold'>Course</Link> </button>
                     </div>
                 </div>
             </div>
